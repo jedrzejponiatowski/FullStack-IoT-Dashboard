26b79354d35ac2197b147bf6fe347594c8a432b8
jedrzejponiatowski <poniatowskijedrzej@gmail.com>
2023-11-14 21:11:56 +0100

    working checkboxes

--- client/src/components/App/Chart.js
+++ client/src/components/App/Chart.js
@@ -27,13 +27,15 @@ const Chart = () => {
   const [chartData, setChartData] = useState([]);
   const [selectedDevices, setSelectedDevices] = useState([]);
   const [uniqueDevices, setUniqueDevices] = useState([]);
+  const [selectedTimeInterval, setSelectedTimeInterval] = useState(null);
   const [chartColors, setChartColors] = useState([
-    theme.palette.primary.main,
-    theme.palette.secondary.main,
-    theme.palette.error.main,
-    theme.palette.warning.main,
-    theme.palette.info.main,
+    '#FF5733', // Pomarańczowy
+    '#34A853', // Zielony
+    '#4285F4', // Niebieski
+    '#EA4335', // Czerwony
   ]);
+
+  
 
   useEffect(() => {
     const fetchData = async () => {
@@ -46,15 +48,15 @@ const Chart = () => {
         const devices = measurementsData.map((measurement) => measurement.device.MAC);
         const uniqueDevices = [...new Set(devices)];
 
-        setUniqueDevices(uniqueDevices);
+        setUniqueDevices(uniqueDevices.slice(0, 4));
 
         // Jeśli nie ma wybranych urządzeń, ustaw wszystkie dostępne jako wybrane
         if (selectedDevices.length === 0) {
-          setSelectedDevices(uniqueDevices.slice(0, 5)); // Ograniczenie do 5 urządzeń
+          setSelectedDevices(uniqueDevices.slice(0, 4)); // Ograniczenie do 4 urządzeń
         }
 
         // Tworzenie danych do wykresu tylko z wybranych urządzeń
-        const chartData = uniqueDevices.slice(0, 5).map((deviceMAC, index) => {
+        const chartData = uniqueDevices.slice(0, 4).map((deviceMAC, index) => {
           return {
             deviceMAC,
             data: measurementsData
@@ -94,6 +96,13 @@ const Chart = () => {
     });
   };
 
+  const handleTimeIntervalChange = (interval) => {
+    // Toggle stanu wybranego interwału czasowego
+    setSelectedTimeInterval((prevSelectedTimeInterval) => {
+      return prevSelectedTimeInterval === interval ? null : interval;
+    });
+  };
+
   return (
     <Grid container spacing={4} style={{ height: '100vh', overflow: 'hidden' }}>
       <CssBaseline />
@@ -110,7 +119,7 @@ const Chart = () => {
             Chart
           </Typography>
           <ResponsiveContainer width="100%" height="100%">
-            <LineChart margin={{ top: 50, right: 16, bottom: 50, left: 24 }}>
+            <LineChart margin={{ top: 50, right: 20, bottom: 50, left: -10 }}>
               <CartesianGrid stroke="#bbb" strokeDasharray="3 3" opacity={0.5} />
               <XAxis dataKey="time" stroke={theme.palette.text.secondary} tick={{ fontSize: 16 }} />
               <YAxis stroke={theme.palette.text.secondary} tick={{ fontSize: 16 }} tickCount={10} domain={[0, 40]} />
@@ -123,6 +132,7 @@ const Chart = () => {
                   data={device.data}
                   name={device.deviceMAC}
                   stroke={device.color}
+                  strokeWidth={2}
                   dot={{ strokeWidth: 2, r: 2 }}
                   connectNulls={true} // Dodane, aby po odznaczeniu checkboxa serii danych zostały usunięte
                   opacity={selectedDevices.includes(device.deviceMAC) ? 1 : 0} // Dodane, aby kontrolować widoczność serii
@@ -153,6 +163,24 @@ const Chart = () => {
                     onChange={() => handleDeviceChange(deviceMAC)}
                   />
                   <ListItemText primary={deviceMAC} />
+                </ListItem>
+                <Divider />
+              </React.Fragment>
+            ))}
+          </List>
+
+          <Typography variant="h6" gutterBottom component="div" style={{ marginTop: '16px' }}>
+            Time Interval
+          </Typography>
+          <List>
+            {['1h', '3h', '6h'].map((interval) => (
+              <React.Fragment key={interval}>
+                <ListItem>
+                  <Checkbox
+                    checked={selectedTimeInterval === interval}
+                    onChange={() => handleTimeIntervalChange(interval)}
+                  />
+                  <ListItemText primary={interval} />
                 </ListItem>
                 <Divider />
               </React.Fragment>
