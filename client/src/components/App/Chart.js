516a647e4062dec3e05b180e76df7247191b2c8a
jedrzejponiatowski <poniatowskijedrzej@gmail.com>
2023-11-18 12:02:07 +0100

    still now working

--- client/src/components/App/Chart.js
+++ client/src/components/App/Chart.js
@@ -14,18 +14,18 @@ import {
 } from '@mui/material';
 import {
   LineChart,
-  Line,
   XAxis,
   YAxis,
   ResponsiveContainer,
   CartesianGrid,
   Legend,
   Tooltip,
-} from 'recharts';
+  Line,
+} from 'recharts';  // Use recharts instead of @mui/x-charts
 
 const Chart = ({ measurementName }) => {
   const theme = useTheme();
-  const [chartData, setChartData] = useState([]);
+  const [chartData, setChartData] = useState({ xAxisData: [], yAxisData: [] });
   const [selectedDevices, setSelectedDevices] = useState([]);
   const [uniqueDevices, setUniqueDevices] = useState([]);
   const [selectedTimeInterval, setSelectedTimeInterval] = useState('1');
@@ -37,20 +37,24 @@ const Chart = ({ measurementName }) => {
     '#EA4335', // Czerwony
   ]);
 
-
   useEffect(() => {
-    fetchData(selectedTimeInterval, measurementName);  // Dodaj measurementName jako drugi argument
+    fetchData(selectedTimeInterval, measurementName);
   }, [selectedDevices, selectedTimeInterval, measurementName]);
 
   const fetchData = async (interval, measurementName) => {
     try {
       const measurementsResponse = await axios.get('/api/measurements');
       const measurementsData = measurementsResponse.data.data;
-  
+
+      const currentTimestamp = new Date().getTime();
+      const intervalMilliseconds = parseInt(interval) * 60 * 1000;
+      const startTime = new Date(currentTimestamp - intervalMilliseconds).toISOString();
+
       const filteredMeasurementsData = measurementsData.filter(
         (measurement) => measurement.channel.type === measurementName
+          && new Date(measurement.timestamp).getTime() >= new Date(startTime).getTime()
       );
-  
+
       if (filteredMeasurementsData.length === 0) {
         const placeholderMeasurement = {
           device: { MAC: 'undefined' },
@@ -59,41 +63,30 @@ const Chart = ({ measurementName }) => {
         };
         filteredMeasurementsData.push(placeholderMeasurement);
       }
-  
-      // Oblicz timestamp, który jest równy teraźniejszemu czasowi minus 3 minuty
-      const intervalMilliseconds = parseInt(interval) * 60 * 1000;
-      const currentTimestamp = new Date().getTime();
-      const startTime = new Date(currentTimestamp - intervalMilliseconds).toISOString();
-  
-      const filteredMeasurements = filteredMeasurementsData.filter(
-        (measurement) => new Date(measurement.timestamp).getTime() >= new Date(startTime).getTime()
-      );
-  
-      filteredMeasurements.sort((a, b) => new Date(a.timestamp) - new Date(b.timestamp));
-  
-      const devices = filteredMeasurements.map((measurement) => measurement.device.MAC);
-      const uniqueDevices = [...new Set(devices)];
-  
+
+      const uniqueDevices = [...new Set(filteredMeasurementsData.map((measurement) => measurement.device.MAC))];
       setUniqueDevices(uniqueDevices.slice(0, 4));
-  
+
       if (selectedDevices.length === 0) {
         setSelectedDevices(uniqueDevices.slice(0, 4));
       }
-  
-      const chartData = uniqueDevices.slice(0, 4).map((deviceMAC, index) => {
+
+      const xAxisData = filteredMeasurementsData.map((measurement) => formatDate(measurement.timestamp));
+
+      const yAxisData = uniqueDevices.map((deviceMAC, index) => {
+        const deviceMeasurements = filteredMeasurementsData
+          .filter((measurement) => measurement.device.MAC === deviceMAC);
+
         return {
           deviceMAC,
-          data: filteredMeasurements
-            .filter((measurement) => measurement.device.MAC === deviceMAC)
-            .map((measurement) => ({
-              time: formatDate(measurement.timestamp),
-              amount: measurement.value !== -99 ? measurement.value : null,
-            })),
+          data: deviceMeasurements.map((measurement) => (
+            measurement.value !== -99 ? measurement.value : null
+          )),
           color: chartColors[index],
         };
       });
-  
-      setChartData(chartData);
+
+      setChartData({ xAxisData, yAxisData });
     } catch (error) {
       console.error('Error fetching data:', error);
     }
@@ -120,8 +113,6 @@ const Chart = ({ measurementName }) => {
     setCurrentAxisInterval(Number(interval) * 2);
   };
 
-
-
   return (
     <Grid container spacing={4} style={{ height: '100vh', overflow: 'hidden' }}>
       <CssBaseline />
@@ -138,27 +129,29 @@ const Chart = ({ measurementName }) => {
             Chart
           </Typography>
           <ResponsiveContainer width="100%" height="100%">
-            <LineChart margin={{ top: 20, right: 10, bottom: 10, left: 0 }}>
+            <LineChart
+              data={chartData.xAxisData.map((time, i) => ({ time, ...chartData.yAxisData.reduce((acc, device) => ({ ...acc, [device.deviceMAC]: device.data[i] }), {}) }))}
+              margin={{ top: 20, right: 10, bottom: 10, left: 0 }}
+            >
               <CartesianGrid stroke="#bbb" strokeDasharray="3 3" opacity={0.5} />
-              <XAxis dataKey="time" stroke={theme.palette.text.secondary} tick={{ fontSize: 16 }}  />
+              <XAxis dataKey="time" stroke={theme.palette.text.secondary} tick={{ fontSize: 16 }} interval={0} />
               <YAxis stroke={theme.palette.text.secondary} tick={{ fontSize: 16 }} tickCount={10} domain={[0, 40]} />
               <Legend verticalAlign="top" height={36} />
-              {/* Dodajemy Tooltip dla interaktywnego podglądu */}
               <Tooltip content={<CustomTooltip />} />
-              {chartData.map((device, index) => (
+
+              {chartData.yAxisData.map((device, index) => (
                 <Line
-                    key={device.deviceMAC}
-                    type="monotone"
-                    dataKey="amount"
-                    data={device.data}
-                    name={device.deviceMAC}
-                    stroke={device.color}
-                    strokeWidth={2}
-                    dot={{ strokeWidth: 2, r: 2 }}
-                    connectNulls={false} // Wyłączamy łączenie null
-                    opacity={selectedDevices.includes(device.deviceMAC) ? 1 : 0}
+                  key={device.deviceMAC}
+                  type="monotone"
+                  dataKey={device.deviceMAC}
+                  name={device.deviceMAC}
+                  stroke={device.color}
+                  strokeWidth={2}
+                  dot={{ strokeWidth: 2, r: 2 }}
+                  connectNulls={false}
+                  opacity={selectedDevices.includes(device.deviceMAC) ? 1 : 0}
                 />
-                ))}
+              ))}
             </LineChart>
           </ResponsiveContainer>
         </Paper>
@@ -213,20 +206,21 @@ const Chart = ({ measurementName }) => {
   );
 };
 
-
-// Komponent CustomTooltip do wyświetlania informacji o punkcie po najechaniu
 const CustomTooltip = ({ active, payload }) => {
-    if (active && payload && payload.length) {
-      const data = payload[0].payload;
-      return (
-        <div style={{ background: '#fff', border: '1px solid #ccc', padding: '10px' }}>
-          <p>{`Time: ${data.time}`}</p>
-          <p>{`Value: ${data.amount}`}</p>
-        </div>
-      );
-    }
-    return null;
-  };
-
+  if (active && payload && payload.length) {
+    const data = payload[0].payload;
+    return (
+      <div style={{ background: '#fff', border: '1px solid #ccc', padding: '10px' }}>
+        <p>{`Time: ${data.time}`}</p>
+        {Object.entries(data)
+          .filter(([key, value]) => key !== 'time' && value !== null)
+          .map(([key, value]) => (
+            <p key={key}>{`${key}: ${value}`}</p>
+          ))}
+      </div>
+    );
+  }
+  return null;
+};
 
 export default Chart;
